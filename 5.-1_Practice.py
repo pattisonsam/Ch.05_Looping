3da1d6731525cfcaf478e48f046ddac387289489
pattisonsam <pattisonsam@urbandaleschools.com>
2022-10-04 09:29:03 -0500

    Guessing game and 5.0 done

---
+++ 5.-1_Practice.py
@@ -0,0 +1,77 @@
+# For loops are used a certain number of time
+# while loops are used till a criteria is met
+import random
+# for i in range(5):
+#     print(i)
+# print("Done")
+#
+# for i in range(1,11): #first value inclusive second value exclusive
+#     print(i)
+# print("Done")
+# for i in range(0,11,2):
+#     print(i)
+# print("Done")
+# for i in range(10,-1,-2):
+#     print(i)
+# print("Done")
+# for i in ["Hello","World"]:
+#     print(i)
+# for i in ("Hello World"):
+#     print(i)
+#
+# total = 0
+# for i in range(1,181):
+#     total += i
+# print(total)
+#
+# i=1
+# while i<2**32:
+#     print(i)
+#     i*=2
+#
+# quit ="n"
+# while quit.lower() == "n":
+#     quit = input("Do you want to quit y/n")
+# print("goodbye")
+#
+# done = False
+# pers = 0
+# while not done:
+#     quit = input("Quit? y/n")
+#     if quit.lower=="y":
+#         done = True
+#     else:
+#         pers += 1
+# print(pers)
+# for i in range(10):
+#     num = random.randrange(100,201,2)
+#     print(num)
+#
+# for i in range(10):
+#     num = random.randint(100,200)
+#     print(num)
+#
+# for i in range(10):
+#     num = random.random()*5+10
+#     print(num)
+done = False
+while not done:
+    num = random.randrange(1, 101)
+    z = 0
+    while not done:
+        x = int(input("Guess the number?"))
+        if x == num:
+            print("You are correct!")
+            print("You took",z,"guesses")
+            i = input("Do you want to play again? y/n")
+            if i == "n":
+                done = True
+            else:
+                num = random.randrange(1, 101)
+                z = 0
+        elif x < num:
+            print("The number is higher")
+            z += 1
+        else:
+            print("The number is lower")
+            z += 1
--- 5.0_Jedi_Training.py
+++ 5.0_Jedi_Training.py
@@ -1,25 +1,24 @@
-  #Sign your name:________________
-
+  #Sign your name:Samuel Pattison
+import random
 '''
  1. Make the following program work.
    '''  
-     print("This program takes three numbers and returns the sum.")
-     total = 0
+# print("This program takes three numbers and returns the sum.")
+# total = 0
+#
+# for i in range(3):
+#     x = int(input("Enter a number: "))
+#     total += x
+# print("The total is:", total)
 
-     for i in range(3):
-         x = input("Enter a number: ")
-         total = total + i
-     print("The total is:", x)
-  
 
 
 '''
   2. Write a Python program that will use a FOR loop to print the even
      numbers from 2 to 100, inclusive.
 '''
-
-
-
+# for i in range(2,101,2):
+#     print(i)
 
 
 '''
@@ -27,19 +26,22 @@
      10 down to, and including, 0. Then print the words Blast off! Remember, use
      a WHILE loop, don't use a FOR loop.
 '''
-
-
-
+# x = 10
+# while x > -1 :
+#     print(x)
+#     x -= 1
+# print("Blast Off")
+#
 
 
 
 '''
   4. Write a program that prints a random integer from 1 to 10 (inclusive).
 '''
-
-
-
-
+# x = random.randrange(1,11)
+# print(x)
+#
+#
 
 
 '''
@@ -52,3 +54,18 @@
      if statements.
       
 '''
+total = 0
+pos = 0
+neg = 0
+equ = 0
+for i in range(7):
+    x = int(input("Enter a number: "))
+    total += x
+    if x > 0:
+        pos += 1
+    elif x < 0:
+        neg += 1
+    else:
+        equ += 1
+print("The total is:", total)
+print("You entered",pos,"positive numbers,",neg,"negative numbers, and",equ,"is the times you entered 0!")
