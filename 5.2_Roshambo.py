9fc1e7fbae7c173a1645e35c2129c4a02ceb1abd
pattisonsam <pattisonsam@urbandaleschools.com>
2022-10-05 12:05:53 -0500

    5.2 done

--- 5.2_Roshambo.py
+++ 5.2_Roshambo.py
@@ -14,7 +14,9 @@ When the user quits print an end game message and their win/loss/tie record
 import random
 print("Welcome to the thunder dome!")
 print("Each time you play you will have a choice, 1 for rock, 2 for paper, 3 for scissors, and 4 for quit.")
-
+win = 0
+lose = 0
+tie = 0
 done = False
 while not done:
     comp = random.randrange(1,4)
@@ -24,34 +26,44 @@ while not done:
         if comp == 1:
             print("The computer chose rock")
             print("You lose")
+            lose += 1
         elif comp == 2:
             print("The computer chose paper")
             print("You win")
+            win += 1
         else:
             print("The computer chose scissors")
             print("You tie")
+            tie += 1
     elif pc == 2:
         print("You chose paper")
         if comp == 1:
             print("The computer chose rock")
             print("You win")
+            win += 1
         elif comp == 2:
             print("The computer chose paper")
             print("You tie")
+            tie += 1
         else:
             print("The computer chose scissors")
             print("You lose")
+            lose +=1
     elif pc == 1:
         print("You chose rock")
         if comp == 1:
             print("The computer chose rock")
             print("You tie")
+            tie += 1
         elif comp == 2:
             print("The computer chose paper")
             print("You lose")
+            lose += 1
         else:
             print("The computer chose scissors")
             print("You win")
+            win += 1
     else:
         done = True
+print("You won",win,"game. You lost",lose,"games. You tied",tie,"times.")
 print("Peace out")
